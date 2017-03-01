7137c93029d0738452d06d47e984b9e27337f4f7
sgaga <a0920336610@gmail.com>
2017-03-01 23:38:02 +0800

    login_20170301_2137
    all-login&nav&hamburger

---
+++ js/javascript.js
@@ -0,0 +1,8 @@
+function ham_show() {
+    document.querySelector('.ham_ul').style.display="block";
+    window.event.cancelBubble = true;
+}
+function ham_close() {
+    document.querySelector('.ham_ul').style.display="none";
+    window.event.cancelBubble = true;
+}
