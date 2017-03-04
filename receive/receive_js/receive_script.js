78fea68aa355afd4596a9722357719b217456bba
sgaga <a0920336610@gmail.com>
2017-03-05 02:25:11 +0800

    20170305_0224_receive
    送船完成(90%)

---
+++ receive/receive_js/receive_script.js
@@ -0,0 +1,39 @@
+// receive_window淡入淡出
+$(document).ready(function(){
+  $(".receive_top").click(function(){
+    $(".receive_window").fadeIn(500);
+    $("#blackbg").fadeIn(500);
+  });
+});
+$(document).ready(function(){
+  $(".window_close").click(function(){
+    $(".receive_window").fadeOut(500);
+    $("#blackbg").fadeOut(500);
+  });
+});
+// 刪除跳入視窗淡入淡出
+$(document).ready(function(){
+  $("#delete").click(function(){
+    $(".delete_window").fadeIn(500);
+    $("#blackbg").fadeIn(500);
+  });
+});
+$(document).ready(function(){
+  $("#delete_no,#delete_yes").click(function(){
+    $(".delete_window").fadeOut(500);
+    $("#blackbg").fadeOut(500);
+  });
+});
+// 檢處跳入視窗淡入淡出
+$(document).ready(function(){
+  $("#report").click(function(){
+    $(".report_window").fadeIn(500);
+    $("#blackbg").fadeIn(500);
+  });
+});
+$(document).ready(function(){
+  $("#report_no,#report_yes").click(function(){
+    $(".report_window").fadeOut(500);
+    $("#blackbg").fadeOut(500);
+  });
+});
