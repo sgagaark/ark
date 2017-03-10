3b49c537bdaa06b30684d4c54647f1ae28be40d4
fish0373 <g112233445598@gmail.com>
2017-03-10 18:13:04 +0800

    連結和一些function

--- send/send_js/send_script.js
+++ send/send_js/send_script.js
@@ -1,38 +1,44 @@
+var globalnum = 0;
 // send_window淡入淡出
 $(document).ready(function(){
   $(".send_top").click(function(){
-    $(".send_window").fadeIn(500);
+    $("#window"+globalnum).fadeIn(500);
     $("#blackbg01").fadeIn(500);
   });
 });
-$(document).ready(function(){
-  $(".window_close").click(function(){
+
+function setNextShow(num) {
+  globalnum = num;
+}
+
+$(document).ready(function () {
+  $(".window_close").click(function () {
     $(".send_window").fadeOut(500);
     $("#blackbg01").fadeOut(500);
   });
 });
 // 刪除跳入視窗淡入淡出
-$(document).ready(function(){
-  $(".delete").click(function(){
+$(document).ready(function () {
+  $(".delete").click(function () {
     $(".delete_window").fadeIn(500);
     $("#blackbg02").fadeIn(500);
   });
 });
-$(document).ready(function(){
-  $("#delete_no,#delete_yes").click(function(){
+$(document).ready(function () {
+  $("#delete_no,#delete_yes").click(function () {
     $(".delete_window").fadeOut(500);
     $("#blackbg02").fadeOut(500);
   });
 });
 // 檢處跳入視窗淡入淡出
-$(document).ready(function(){
-  $(".report").click(function(){
+$(document).ready(function () {
+  $(".report").click(function () {
     $(".report_window").fadeIn(500);
     $("#blackbg02").fadeIn(500);
   });
 });
-$(document).ready(function(){
-  $("#report_no,#report_yes").click(function(){
+$(document).ready(function () {
+  $("#report_no,#report_yes").click(function () {
     $(".report_window").fadeOut(500);
     $("#blackbg02").fadeOut(500);
   });
